0536a1fb6e94590e1f8a80eeb66f92182d7d1e3c
Amanda Grooms <amandafgrooms@gmail.com>
2019-11-08 10:34:25 -0600

    added basic user stories

---
+++ script.js
@@ -0,0 +1,83 @@
+//sets variables to zero
+let currentQuestion = 0
+let score = 0
+
+function loadApp() {
+  //This function will create and load the main page for my
+  //wine quiz in the DOM.
+  console.log('`loadApp` ran')
+}
+
+function startWineQuiz() {
+  //This function will start the quiz when user clicks
+  //start button
+  console.log('`startWineQuiz` ran')
+}
+
+function updateQuestion() {
+  //This function updates the question number
+  console.log('`updateQuestion` ran')
+}
+
+function updateScore() {
+  //This function updates the score; It will increment the
+  //score based on user response
+  console.log('`updateScore` ran')
+}
+
+function renderQuestion() {
+  //This function will render the first question
+  console.log('`renderQuestion` ran')
+}
+
+function updateAnswers() {
+  //This function will render the answer options for the
+  //current question
+  console.log('`updateAnswers` ran')
+}
+
+function submitAnswer() {
+  //This function submits a selected answer and checks it against
+  //the correct answer. It then runs answer functions accordingly.
+  console.log('`submitAnswer` ran')
+}
+
+function correctAnswer() {
+  //This function will show feedback if the user selects the
+  //correct answer
+  console.log('`correctAnswer` ran')
+}
+
+function wrongAnswer() {
+  //This function will show feedback if the users selects the
+  //wrong answer
+  console.log('`wrongAnswer` ran')
+}
+
+function nextQuestion() {
+  //This function will render the next question
+  console.log('`nextQuestion` ran')
+}
+
+function finalScore() {
+  //This function will deplay user's final score and give
+  //feedback based on how many answers they got correct
+  console.log('`finalScore` ran')
+}
+
+function restartQuiz() {
+  //This function will take user back to main page
+  //so they can restart the quiz
+}
+
+function makeQuiz() {
+  loadApp()
+  startWineQuiz()
+  renderQuestion()
+  updateAnswers()
+  nextQuestion()
+  finalScore()
+  restartQuiz()
+}
+
+$(makeQuiz)
